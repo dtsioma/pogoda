f78bb624791950386b54668ef878d5874d0fa79f
Daniil Tsioma <d.tsioma@gmail.com>
2021-07-27 16:15:35 -0700

    Added fonts

---
+++ src/utils/themeConfig.js
@@ -0,0 +1,21 @@
+import BalooChettan2Bold from "../static/BalooChettan2Bold.ttf";
+import BalooChettan2Regular from "../static/BalooChettan2Regular.ttf";
+import ZenTokyoZooRegular from "../static/ZenTokyoZooRegular.ttf";
+
+export const themeConfig = {
+  typography: {
+    fontFamily: [
+      "Baloo Chettan 2",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+  },
+};
