8dab170e919327441a449b0746aad776331e568f
Daniil Tsioma <d.tsioma@gmail.com>
2021-07-27 16:16:25 -0700

    Removed unnecessary theme config

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import { createTheme, ThemeProvider } from "@material-ui/core/styles";
-import { themeConfig } from "./utils/themeConfig";
 import "./App.css";
 
 function App() {
-  const theme = createTheme(themeConfig);
-  return (
-    <ThemeProvider theme={theme}>
-      <div className="App"></div>
-    </ThemeProvider>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
