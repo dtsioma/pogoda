f725ccd28f43a438a44ea7a56c95eb31f98bb2eb
Daniil Tsioma <d.tsioma@gmail.com>
2021-07-28 12:23:27 -0700

    added backend

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello!"));
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
