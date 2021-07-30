5441dccc7a0f03acb652bc95bece01507b1ce557
Daniil Tsioma <d.tsioma@gmail.com>
2021-07-29 19:38:49 -0700

    added Back button

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,5 +1,12 @@
-import { Theme, AppBar, Toolbar, makeStyles, Link } from "@material-ui/core";
-import { Link as RouterLink } from "react-router-dom";
+import {
+  Theme,
+  AppBar,
+  Toolbar,
+  makeStyles,
+  Link as MaterialLink,
+} from "@material-ui/core";
+import { Link as RouterLink, useLocation } from "react-router-dom";
+import KeyboardBackspaceIcon from "@material-ui/icons/KeyboardBackspace";
 import GitHubIcon from "@material-ui/icons/GitHub";
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -14,10 +21,22 @@ const useStyles = makeStyles((theme: Theme) => ({
     color: theme.palette.primary.contrastText,
     marginLeft: "auto",
   },
+  back: {
+    display: "flex",
+    alignItems: "center",
+    color: theme.palette.primary.contrastText,
+    textDecoration: "none",
+    marginLeft: "4em",
+    padding: "5px",
+  },
+  icon: {
+    marginRight: "5px",
+  },
 }));
 
 const Header = () => {
   const classes = useStyles();
+  const { pathname } = useLocation();
 
   return (
     <AppBar>
@@ -25,13 +44,18 @@ const Header = () => {
         <RouterLink to="/" className={classes.title}>
           pogoda
         </RouterLink>
-        <Link
+        {pathname !== "/" ? (
+          <RouterLink to="/" className={classes.back}>
+            <KeyboardBackspaceIcon className={classes.icon} /> back
+          </RouterLink>
+        ) : null}
+        <MaterialLink
           href={process.env.REACT_APP_GITHUB_LINK!}
           className={classes.link}
           target="_blank"
         >
           <GitHubIcon />
-        </Link>
+        </MaterialLink>
       </Toolbar>
     </AppBar>
   );
